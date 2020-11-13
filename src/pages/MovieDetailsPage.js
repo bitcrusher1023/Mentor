acab03733cdec0450c1fcb0aacbb0bd3f7b0c77d
RichardHan0314 <shinystar0314@gmail.com>
2020-11-14 01:46:54 +0300

    fix: some fixes and small example with data fetch

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import useSWR from 'swr';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { getAllMovies } from '../store/selectors';
+import { fetchMovieById } from '../store/actions/moviesActions';
+import { getAllMovies} from '../store/selectors';
 import MovieDetails from '../components/MovieDetails';
 import ActionBar from '../components/ActionBar';
 import MoviesList from '../components/MoviesList';
+import { API_GATEWAY } from '../store/actions/moviesActions';
+
+const fetcher = url => axios.get(url).then(({ data }) => data);
 
 const MovieDetailsPage = () => {
     const { id } = useParams();
-    const movies = useSelector(getAllMovies);
+    // useSWR it's a hook for data fetching you can read more about https://github.com/vercel/swr
+    const { data, error } = useSWR(`${API_GATEWAY}/${id}`, fetcher);
+    // Also you can use redux flow ti request data by movie id from url
+    // const dispatch = useDispatch();
+    // // const movies = useSelector(getAllMovies);
 
-    const getUserById = (movieId) => movies.find(movie => movie.id === movieId);
+    // useEffect(() => {
+    //     dispatch(fetchMovieById(id));
+    // }, [dispatch, id])
+    // const getUserById = (movieId) => movies.find(movie => movie.id === movieId);
 
-    const movieDetailsData = getUserById(parseInt(id, 10));
+    // const movieDetailsData = getUserById(parseInt(id, 10));
+    if (error) return 'error';
+
+    console.log(data);
 
     return (
         <Wrapper>
-            <MovieDetails movieData = {movieDetailsData} />
-            <MovieContainerWrapper>
+            { data ? <MovieDetails movieData={data} /> : 'loading'}
+            {/* In previous tasks we alreade have a container we can use this container here
+            and all data nedded in this container will be automatically request base on router params. And this components
+            will be independant on that data */}
+            {/* <MovieContainerWrapper>
                 <ActionBar />
                 <MoviesList movies={movies}/>
-            </MovieContainerWrapper>
+            </MovieContainerWrapper> */}
         </Wrapper>
     );
 };
